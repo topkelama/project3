a09efddbedfdfc2f48eda0313e0e69ebc7dfdfd1
Chhiring <ctsgoodwill@yahoo.com>
2024-03-14 21:42:25 -0400

    sql_V2

--- ds_salaries.sql
+++ ds_salaries.sql
@@ -1,8 +1,8 @@
 create DATABASE job_listing;
-CREATE INDEX idx_company_location ON Location (company_location);
-SET SQL_SAFE_UPDATES = 0;
+/*CREATE INDEX idx_company_location ON Location (company_location);
+SET SQL_SAFE_UPDATES = 0;*/
 
-CREATE TABLE Employee (
+/*CREATE TABLE Employee (
     employee_id INT AUTO_INCREMENT PRIMARY KEY,
     work_year INT,
     experience_level VARCHAR(50),
@@ -22,7 +22,7 @@ CREATE TABLE Job (
     salary_currency VARCHAR(10),
     salary_in_usd INT
     
-);
+);*/
 
 CREATE TABLE Company (
     company_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -37,7 +37,44 @@ CREATE TABLE Location (
     employee_residence varchar(100)
 );
 
-INSERT INTO Employee (work_year, experience_level, employment_type, employee_residence, remote_ratio)
+CREATE TABLE job (
+    job_title_id INT AUTO_INCREMENT PRIMARY KEY,
+    work_year INT,
+    experience_level VARCHAR(50),
+    employment_type VARCHAR(50),
+    employee_residence varchar(100),
+    remote_ratio INT,
+    company_location VARCHAR(100),
+    company_size varchar(100),
+    job_title VARCHAR(100),
+    salary_id INT,
+    FOREIGN KEY (salary_id) REFERENCES salary(salary_id)
+);
+
+CREATE TABLE salary (
+    salary_id INT AUTO_INCREMENT PRIMARY KEY,
+    salary int,
+    salary_in_usd INT,
+    salary_currency VARCHAR(10)
+);
+SELECT *
+FROM job;
+
+INSERT INTO job (work_year, experience_level, employment_type, employee_residence, remote_ratio, company_location, company_size, job_title, salary_id)
+SELECT DISTINCT f.work_year, f.experience_level, f.employment_type, f.employee_residence, f.remote_ratio, f.company_location, f.company_size, f.job_title, s.salary_id
+FROM file AS f
+JOIN salary AS s ON f.salary = s.salary AND f.salary_in_usd = s.salary_in_usd AND f.salary_currency = s.salary_currency;
+
+INSERT INTO salary (salary, salary_in_usd, salary_currency)
+SELECT DISTINCT salary, salary_in_usd, salary_currency
+FROM file;
+
+INSERT INTO job (work_year, experience_level, employment_type, employee_residence, remote_ratio, company_location, company_size, job_title, salary_id)
+SELECT DISTINCT work_year, experience_level, employment_type, employee_residence, remote_ratio, company_location, company_size, job_title, salary_id
+FROM file;
+
+
+/*INSERT INTO Employee (work_year, experience_level, employment_type, employee_residence, remote_ratio)
 SELECT work_year, experience_level, employment_type, employee_residence, remote_ratio
 FROM file;
 
@@ -51,10 +88,10 @@ FROM file;
 
 INSERT INTO Company (company_location, company_size )
 SELECT DISTINCT company_location, company_size
-FROM file;
+FROM file;*/
 -- where values are null
 -- ++++++++++++++++++++++++++++++++++++++++++++++++++++++
-UPDATE employee e
+/*UPDATE employee e
 JOIN job j ON e.job_title_id = j.job_title_id
 SET e.job_title_id = j.job_title_id;
 
@@ -64,4 +101,4 @@ UPDATE employee e
 JOIN location l ON e.employee_residence = l.company_residence
 SET e.location_id = l.location_id;
 -- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-ALTER TABLE location CHANGE COLUMN company_residence employee_residence VARCHAR(100);
+ALTER TABLE location CHANGE COLUMN company_residence employee_residence VARCHAR(100);*/
